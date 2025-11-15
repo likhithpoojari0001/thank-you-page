136c9a35664cf88dd3e449fa910fe9bada9a2105
likhithpoojari0001 <likhithpoojari07@gmail.com>
2025-11-16 00:38:45 +0530

    Initial commit - Thank You Angular Test

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+
+export const routes: Routes = [
+  { path: '', component: ThankYouComponent }
+];
+
